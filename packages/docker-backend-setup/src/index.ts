2146733cba50ca58d32d7f8bc22a53a1ee614eb7
Leonardo Roese <leo.roese@hotmail.com>
2021-06-18 15:23:23 -0600

    feat: Added quick docker setup project with compose

--- packages/docker-backend-setup/src/index.ts
+++ packages/docker-backend-setup/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv-safe';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -8,8 +9,14 @@ const port = 3000;
 
 console.log('name is', process.env.NAME);
 
-app.get('/', (_req: express.Request, res: express.Response) => {
-  res.send('Hello World!');
+app.get('/boop', async (_req: express.Request, res: express.Response) => {
+  try {
+    const response = await fetch('http://server2:3000/sup');
+    const data = await response.json();
+    res.send(data);
+  } catch (err) {
+    console.log('error ', err);
+  }
 });
 
 app.listen(port, () => {
