6a251cb2b58367607dd79e682de4c21caa030a84
Leonardo Roese <leo.roese@hotmail.com>
2021-06-25 06:32:49 -0600

    feat: federation and gateway setup

---
+++ packages/apollo-federation-setup/gateway/src/index.ts
@@ -0,0 +1,33 @@
+import { ApolloGateway } from '@apollo/gateway';
+import { ApolloServer } from 'apollo-server';
+import dotenv from 'dotenv-safe';
+
+dotenv.config();
+
+const startServer = () => {
+  const gateway = new ApolloGateway({
+    serviceList: [
+      {
+        name: 'books',
+        url: process.env.BOOKS_SERVICE_URL,
+      },
+    ],
+  });
+
+  const server = new ApolloServer({
+    gateway,
+    playground: process.env.NODE_ENV !== 'production',
+    subscriptions: false,
+  });
+
+  server
+    .listen({
+      port: 4000,
+    })
+    .then(({ url }) => {
+      console.log(`🚀  Server ready at ${url}graphql`);
+    })
+    .catch(err => console.log('Error launching server', err));
+};
+
+startServer();
