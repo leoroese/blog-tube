e99094f33ecec88e21f5257d336119a132ffcc5d
Leonardo Roese <leo.roese@hotmail.com>
2021-07-23 08:12:56 -0600

    feat: update dependencies, fix reference error due to new jest version

--- packages/next-ts-frontend/jest.config.js
+++ packages/next-ts-frontend/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/coverage', '<rootDir>/dist'],
   moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/src', '<rootDir>/pages'],
